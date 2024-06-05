3669a24e97652e2dc957d24ad211b49b0cec87a8
McPherson <joanna.mcpherson@cvent.com>
2024-06-05 16:53:05 -0400

    set up basic page navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,26 @@
 import './App.css';
 import 'react-bootstrap';
-import { Header } from "./components/Header";
-import { Content } from "./components/Content";
-import { Footer } from "./components/Footer";
-import { Signup } from "./components/Signup";
-import { Login } from "./components/Login";
-import { LogoutLink } from "./components/LogoutLink";
+import { Header } from './components/Header';
+import { Content } from './components/Content';
+import { Footer } from './components/Footer';
+import { Signup } from './components/Signup';
+import { Login } from './components/Login';
+import { LogoutLink } from './components/LogoutLink';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Header />
       <Content />
-      <Footer />
-      <Signup /> 
-      <Login />
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <LogoutLink />
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
