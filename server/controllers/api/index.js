fb546bb3e5c8c8e7fa55ffba3a92eba57b197d5d
Andrew, Muhn <Andrew.Muhn@cvent.com>
2024-06-06 09:09:44 -0700

    added CRUD daycare Routes

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
+const dayCareRoutes = require('./dayCareRoutes');
 
 router.use('/users', userRoutes);
+router.use('/daycare', dayCareRoutes);
 
 module.exports = router;
