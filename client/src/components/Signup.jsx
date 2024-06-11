ae4d99e535f3dc5e497008fa0fc562f408f083bf
McPherson <joanna.mcpherson@cvent.com>
2024-06-11 13:51:26 -0400

    errors

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
 import { postNewUser } from '../utils/api/userCalls';
 
 export function Signup() {
-  const [setErrors] = useState([]);
-
   const handleSubmit = (event) => {
     event.preventDefault();
-    setErrors([]);
+
     const formData = new FormData(event.target);
     const params = Object.fromEntries(formData.entries());
     postNewUser(params)
@@ -18,10 +15,8 @@ export function Signup() {
       .catch((error) => {
         if (error.response && error.response.data.errors) {
           console.log(error.response.data.errors);
-          setErrors(error.response.data.errors);
         } else {
           console.log(error);
-          setErrors(['An error occurred. Please try again.']);
         }
       });
   };
@@ -29,28 +24,44 @@ export function Signup() {
   return (
     <div id="signup">
       <h1>Signup</h1>
-      <ul>
-        {/* {errors.map((error) => (
-          <li key={error}>{error}</li>
-        ))} */}
-      </ul>
       <div className="form-container">
         <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="exampleInputName">Name</label>
-            <input name="name" type="text" className="form-control" id="exampleInputName" required />
+            <input
+              name="name"
+              type="text"
+              className="form-control"
+              id="exampleInputName"
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email address</label>
-            <input name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+            <input
+              name="email"
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Password</label>
-            <input name="password" type="password" className="form-control" id="exampleInputPassword1" required />
+            <input
+              name="password"
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              required
+            />
           </div>
-          <button type="submit" className="my-btn btn-primary">Submit</button>
+          <button type="submit" className="my-btn btn-primary">
+            Submit
+          </button>
         </form>
-      </div>  
+      </div>
     </div>
   );
 }
