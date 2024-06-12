58f87e3e41ea491173b2ffdbadca49080b723b46
Andrew, Muhn <Andrew.Muhn@cvent.com>
2024-06-12 08:27:21 -0700

    removed uneccesary setup and added get all users test

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -1,26 +1,24 @@
 const request = require('supertest');
 const app = require('../server');
-const { mockUser, hashPassword, setupDb } = require('./utils');
+const { mockUser } = require('./utils');
 
 describe('Authentication Endpoints', () => {
-    beforeEach(async () => {
-        await setupDb();
-    });
+  it('should get all users', async () => {
+    const res = await request(app).get('/api/users');
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('Get all users api call!');
+  });
 
-    it('should create a new user', async () => {
-        const hashedPassword = await hashPassword(mockUser.password);
-        const newUser = {
-            name: mockUser.name,
-            email: mockUser.email,
-            password: hashedPassword,
-        };
+  it('should create a new user', async () => {
+    const newUser = {
+      name: mockUser.name,
+      email: mockUser.email,
+      password: mockUser.password,
+    };
 
-        const res = await request(app)
-            .post('/api/users')
-            .send(newUser);
+    const res = await request(app).post('/api/users').send(newUser);
 
-        expect(res.status).toBe(201);
-        expect(res.text).toMatch(/User added with ID: \d+/);
-    });
-
+    expect(res.status).toBe(201);
+    expect(res.text).toMatch(/User added with ID: \d+/);
+  });
 });
